a908b55677b3eaa2983f029e9eafb201a7a8d3ac
Bruno Lippert <brunolippert2002@gmail.com>
2023-10-25 11:11:11 -0300

    feat: add navbar component

---
+++ src/components/nav/Navbar.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import * as S from "./styles";
+import { useRouter } from "next/router";
+
+export default function Navbar() {
+    const [currentUser, setCurrentUser] = useState<string>("TechCompany");
+    const [currentCompany, setCurrentCompany] = useState<string>("Bruno Lippert");
+  
+    const router = useRouter();
+  
+    // useEffect(() => {
+    //   setCurrentUser(localStorage.getItem("currentUser"))
+    //   setCurrentCompany(localStorage.getItem("currentCompanyName"))
+    // })
+    
+    const logout = () => {
+      localStorage.clear();
+      router.push("/Login");
+    };
+  
+    return (
+      <S.Container>
+        <S.Nav>
+          <S.NavItensContainer>
+            <S.Navitens>
+              <p>Config</p>
+            </S.Navitens>
+            <S.Navitens>
+              <p>Clientes</p>
+            </S.Navitens>
+            <S.Navitens>
+              <p>Produtos</p>
+            </S.Navitens>
+            <S.Navitens>
+              <p>Pedidos</p>
+            </S.Navitens>
+          </S.NavItensContainer>
+  
+          <S.NavLoginsContainer>
+              <S.logout>
+                <input type="submit" value="Sair" onClick={logout} />
+              </S.logout>
+            <S.Navlogin>
+              <S.user>{currentUser && <p>{currentUser}</p>}</S.user>
+              <S.company>{currentCompany && <p>{currentCompany}</p>}</S.company>
+            </S.Navlogin>
+          </S.NavLoginsContainer>
+        </S.Nav>
+  
+        <S.Main></S.Main>
+      </S.Container>
+    );
+}
---
+++ src/components/nav/styles.ts
@@ -0,0 +1,106 @@
+import styled from 'styled-components';
+import { colors, fonts } from '../../styles/themes/theme';
+
+export const Container = styled.div`
+    *{
+        margin: 0;
+        padding: 0;
+    }
+
+    background-color: ${colors.quinary};
+    height: 100vh;
+`; 
+
+export const Nav = styled.nav`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin: 0 20px 20px 20px;
+    height: 70px;
+`;
+
+
+export const NavItensContainer = styled.section`
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+    width: 70%;
+`;
+export const Navitens = styled.article`
+    background-color: ${colors.primary};
+    display: flex;
+    flex: 1;
+
+    max-width: 200px;
+
+    border-top-right-radius: 0;
+    border-top-left-radius: 0;
+    border-bottom-right-radius: 15px;
+    border-bottom-left-radius: 15px;
+
+    p {
+        font-size: 1.5em;
+        font-family: ${fonts.arial};
+        margin: auto;
+        text-align: center;
+        color: #fff;
+    }
+
+`;
+
+export const NavLoginsContainer = styled.section`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    flex-direction: row;
+    gap: 20px;
+
+    background-color: ${colors.primary};
+    display: flex;
+    flex: 1;
+
+    max-width: 250px;
+    
+    border-top-right-radius: 0;
+    border-top-left-radius: 0;
+    border-bottom-right-radius: 15px;
+    border-bottom-left-radius: 15px;
+
+    input[type=submit] {
+        border: none;
+        color: #fff;
+        background-color: ${colors.primary};
+        width: 60px;
+        cursor: pointer;
+        font-size: 1em;
+
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+`;
+export const Navlogin = styled.article`
+    display: flex;
+    flex-direction: column;
+    gap: 7px;
+
+    p{
+        font-family: ${fonts.arial};
+        margin: auto;
+        text-align: center;
+        color: #fff;
+    }
+`;
+export const logout = styled.div`
+
+`;
+export const company = styled.div`
+
+`;
+export const user = styled.div`
+
+`;
+
+export const Main = styled.main`
+
+`;
