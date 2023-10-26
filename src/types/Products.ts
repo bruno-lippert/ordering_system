e3ce277e40708c31d1d890a6863bcdbd4e8db063
Bruno Lippert <brunolippert2002@gmail.com>
2023-10-26 17:25:11 -0300

    feat: created product type

---
+++ src/types/Products.ts
@@ -0,0 +1,9 @@
+export type User = {
+    id?: string,
+    name: string,
+    idcompany: number, 
+    description: string,
+    stockquantity: number,
+    price: number,
+    unitofmeasure: number
+}
