b34f1af31f40fc43bdd8dea3534a6ea3843bec6d
Bruno Lippert <brunolippert2002@gmail.com>
2023-10-30 22:52:12 -0300

    update

---
+++ src/types/Product.ts
@@ -0,0 +1,9 @@
+export type Product = {
+    id?: string,
+    name: string,
+    idcompany: number, 
+    description: string,
+    stockquantity: number,
+    price: number,
+    unitofmeasure: number
+}
