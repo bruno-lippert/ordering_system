645c1111d901451971c4210313d95ff1cc0d7a81
Bruno Lippert <brunolippert2002@gmail.com>
2023-11-02 10:11:24 -0300

    feat: error handling

--- src/services/productsManagement.ts
+++ src/services/productsManagement.ts
@@ -1,11 +1,18 @@
 import supabase from "../database";
 import { Product } from "../types/Product";
 
-export async function getProducts(idCompany: string) {
-  let { data: products, error } = await supabase
+export async function getProductsByIDCompany(idCompany: string) {
+  let { data, error } = await supabase
     .from("products")
     .select("*")
     .eq("idcompany", idCompany);
+
+    if (error) {
+      console.log("Erro ao buscar produtos:", error);
+      return null;
+    }
+  
+    return data;
 }
 
 export async function addProduct(prod : Product) {
@@ -13,4 +20,11 @@ export async function addProduct(prod : Product) {
     .from("products")
     .insert(prod)
     .select();
+
+    if (error) {
+      console.log("Erro ao cadastrar produtos:", error);
+      return null;
+    }
+
+    return data;
 }
