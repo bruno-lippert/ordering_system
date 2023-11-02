69fbe155fafe5bdee915c45b2bbad93c5eecf6c2
Bruno Lippert <brunolippert2002@gmail.com>
2023-11-02 19:59:33 -0300

    update

--- pages/System.tsx
+++ pages/System.tsx
@@ -5,12 +5,6 @@ import Navbar from "../src/components/nav/Navbar";
 
 export default function System() {
 
-  // useEffect(() => {
-  //   setCurrentUser(localStorage.getItem("currentUser"))
-  //   setCurrentCompany(localStorage.getItem("currentCompanyName"))
-  // })
-
-
   return (
     <S.Container>
       <Navbar />
