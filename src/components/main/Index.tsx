c2708928ac36cae5b3d750d97877f784d99c9da1
Bruno Lippert <brunolippert2002@gmail.com>
2023-11-02 20:03:07 -0300

    ipdate

--- src/components/main/Index.tsx
+++ src/components/main/Index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import * as S from './styles'
+import { Products } from '../products'
 
 export default function Main() {
   return (
     <S.Container>
-      <S.Content></S.Content>
+      <S.Content>
+        <Products />
+      </S.Content>
     </S.Container>
   )
 }
--- src/types/Products.ts
+++
@@ -1,9 +0,0 @@
-export type User = {
-    id?: string,
-    name: string,
-    idcompany: number, 
-    description: string,
-    stockquantity: number,
-    price: number,
-    unitofmeasure: number
-}
