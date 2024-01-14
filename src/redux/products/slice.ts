e684136ba516cfbdf62410b271766cb3469dfee8
Bruno Lippert <brunolippert2002@gmail.com>
2024-01-14 18:36:47 -0300

    update

--- src/redux/products/slice.ts
+++ src/redux/products/slice.ts
@@ -1,28 +1,34 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Product } from "../../types/Product";
 
 interface ProductsState {
-    products: Product[];
+  products: Product;
+}
+
+const initialState: ProductsState = {
+  products: {
+    id: '',
+    idcompany: null,
+    description: '',
+    price: null,
+    stockquantity: null,
+    unitofmeasure: ''
   }
-  
-  const initialState: ProductsState = {
-    products: [],
-  };
+};
 
 const productsSlice = createSlice({
-    name: 'products',
-    initialState,
-    reducers: {
-        setProducts: (state, action: PayloadAction<Product[]>) => {
-            state.products = action.payload;
-          },
-        addProducts: (state, action: PayloadAction<Product>) => {
-            state.products.push(action.payload)
-        }
+  name: "products",
+  initialState,
+  reducers: {
+    setSelectedProduct: (state, action: PayloadAction<Product>) => {
+      state.products = action.payload;
+    },
+    // addProducts: (state, action: PayloadAction<Product>) => {
+    //     state.products.push(action.payload)
+    // }
+  },
+});
 
-    }
-})
+export const { setSelectedProduct } = productsSlice.actions;
 
-export const { addProducts, setProducts } = productsSlice.actions
-
-export default productsSlice.reducer
+export default productsSlice.reducer;
