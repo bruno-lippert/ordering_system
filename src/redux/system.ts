7dcbbe66be5d99c0a1e746fde835e571ed1d0d6d
Bruno Lippert <brunolippert2002@gmail.com>
2024-01-30 21:40:18 -0300

    update

--- src/redux/system.ts
+++ src/redux/system.ts
@@ -1,12 +1,12 @@
 import { applyMiddleware } from "redux";
 import { configureStore } from "@reduxjs/toolkit"
-import logger from 'redux-logger';
+//import logger from 'redux-logger';
 
 import rootReducer from "./root-reducer";
 
 const store = configureStore({
     reducer: rootReducer,
-    middleware: [logger]
+    //middleware: [logger]
 });
 
 export default store;
