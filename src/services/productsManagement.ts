7f25a3ff43ea20cdb938788ecfe62d9824f81cc4
Bruno Lippert <brunolippert2002@gmail.com>
2024-01-30 21:50:55 -0300

    criado funcionalidade de deletar produtos ao sistema

--- src/services/productsManagement.ts
+++ src/services/productsManagement.ts
@@ -8,39 +8,48 @@ export async function getProductsByIDCompany(idCompany: string) {
     .eq("idcompany", idCompany)
     .order("id");
 
-    if (error) {
-      console.log("Erro ao buscar produtos:", error);
-      return null;
-    }
-  
-    return data;
+  if (error) {
+    console.log("Erro ao buscar produtos:", error);
+    return null;
+  }
+
+  return data;
 }
 
 export async function createProduct(prod: Product) {
-  const { data, error } = await supabase
-    .from("products")
-    .insert(prod)
-    .select();
+  const { data, error } = await supabase.from("products").insert(prod).select();
 
-    if (error) {
-      console.log("Erro ao cadastrar produtos:", error);
-      return null;
-    }
+  if (error) {
+    console.log("Erro ao cadastrar produtos:", error);
+    return null;
+  }
 
-    return data;
+  return data;
 }
 
 export async function updatePtoduct(prod: Product, idProd: string) {
   const { data, error } = await supabase
-        .from('products')
-        .update(prod)
-        .eq('id', idProd)
-        .select()
+    .from("products")
+    .update(prod)
+    .eq("id", idProd)
+    .select();
 
-    if(error) {
-        console.error('Erro ao editar:', error.message);
-        return;
-    }
+  if (error) {
+    console.error("Erro ao editar:", error.message);
+    return;
+  }
 
-    return data
+  return data;
+}
+
+export async function deletePtoduct(idProd: string) {
+  const { error } = await supabase
+    .from("products")
+    .delete()
+    .eq("id", idProd);
+
+  if (error) {
+    console.error("Erro ao editar:", error.message);
+    return;
+  }
 }
