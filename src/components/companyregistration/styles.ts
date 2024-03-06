43072807f145e02d45134cc6c14c9f4bd054962a
Bruno Lippert <brunolippert2002@gmail.com>
2024-03-05 21:08:19 -0300

    feat: adicionado validador de cnpj e cep

--- src/components/companyregistration/styles.ts
+++ src/components/companyregistration/styles.ts
@@ -1,82 +1,83 @@
-import styled from 'styled-components';
-import { colors, fonts } from '../../styles/themes/theme';
+import styled from "styled-components";
+import { colors, fonts } from "../../styles/themes/theme";
 
 export const Container = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-    height: 100vh;
-    background-color: ${colors.secondary};
+  height: 100vh;
+  background-color: ${colors.secondary};
 `;
 
-export const Company = styled.main `
+export const Company = styled.main`
+  * {
+    margin: 0;
+    padding: 0;
+  }
 
-    *{
-        margin: 0;
-        padding: 0;
-    }
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 0 20px;
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
+  height: 550px;
+  width: 400px;
+  background-color: ${colors.primary};
+
+  border-radius: 10px;
+
+  gap: 10px;
+
+  h1 {
+    margin-bottom: 20px;
+  }
+
+  .inputsContainer {
     margin: 0 20px;
+    width: 85%;
+  }
 
-    height: 500px;
-    width: 400px;
-    background-color: ${colors.primary};
+  input[type="text"],
+  input[type="number"],
+  select {
+    border-radius: 5px;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    border: none;
+    padding: 2px;
+    background-color: ${colors.secondary};
+  }
+  input[type="submit"] {
+    border-radius: 5px;
+    padding: 5px;
+    margin: 15px 0 5px 0;
+    background-color: #fff;
+    border: none;
 
-    border-radius: 10px;
+    &:hover {
+      cursor: pointer;
+    }
 
-    gap: 10px;
+    &:active {
+      background-color: ${colors.tertiary};
+    }
+  }
 
+  @media (max-width: 350px) {
     h1 {
-        margin-bottom: 20px;
+      font-size: 1.6em;
     }
+  }
 
-    .inputsContainer {
-        margin: 0 20px;
-        width: 85%;
-    }
-
-    input[type="text"], select {
-        
-        border-radius: 5px;
-        display: flex;
-        justify-content: space-between;
-        width: 100%;
-        border: none;
-        padding: 2px;
+  @media (max-width: 270px) {
+    h1 {
+      text-align: center;
     }
     input[type="submit"] {
-        border-radius: 5px;
-        padding: 5px;
-        margin: 15px 0 5px 0;
-        background-color: #fff;
-        border: none;
-
-        &:hover {
-            cursor: pointer;
-        }
-
-        &:active {
-            background-color: ${colors.tertiary};
-        }
+      margin: 5px 0;
     }
-
-    @media(max-width: 350px) {
-        h1 {
-            font-size: 1.6em;
-        }
-    }
-
-    @media(max-width: 270px) {
-        h1 {
-            text-align: center;
-        }
-        input[type="submit"] {
-            margin: 5px 0;
-        }
-    }
+  }
 `;
