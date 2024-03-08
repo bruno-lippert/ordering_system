a7aed4f1389eb90750d7580bf996bc9d11a53a2f
Bruno Lippert <brunolippert2002@gmail.com>
2024-03-07 22:19:39 -0300

    update

--- pages/CompanyRegistration.tsx
+++ pages/CompanyRegistration.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Index from "../src/components/companyregistration";
+import CompanyRegistrationProvider from "../src/context/CompanyRegistrationContext";
 
 export default function companyRegistration() {
   return (
-    <>
+    <CompanyRegistrationProvider>
       <Index />
-    </>
+    </CompanyRegistrationProvider>
   );
 }
